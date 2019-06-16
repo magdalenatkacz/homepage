5a0c869d824ec6d70325eb1586db4848474d3509
Magda <ffodhla@gmail.com>
2019-06-16 12:35:50 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Hej mordeczki!');
+const name='Magda';
+const age=26;
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+alert("Nie uwazalam na lekcji, dlateo nie umiem tego obsluzyc JS");
